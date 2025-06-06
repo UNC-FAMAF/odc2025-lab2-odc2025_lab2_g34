a00ab2eb19ac1077c9ed501b0f8079fa7656478f
Kiefer <kiefer.arndt@mi.unc.edu.ar>
2025-06-06 18:06:09 -0300

    animacion terminada e incorporado el logo odc2025

--- ejercicio2/app.s
+++ ejercicio2/app.s
@@ -1,53 +1,132 @@
+	.data
+	pos_gotas: .word 110,15, 220,30, 240,50, 62,52, 302,21, 380,27, 156,37, 130,93, 206,105, 295,105, 29,151, 83,147, 147,157, 112,210, 53,220, 180,177, 229,143, 346,76, 423,56, 400,110, 374,166, 257,199, 213,235, 460,141, 478,16, 544,45, 603,9, 585,95, 526,187, 483,220, 414,232, 560,206, 610,166, 601,244 
+	
+	pos_gotas_frame2: .word 110,35, 220,50, 240,70, 62,72, 302,41, 380,47, 156,57, 130,113, 206,125, 295,125, 29,171, 83,167, 147,177, 112,230, 53,240, 180,197, 229,163, 346,96, 423,76, 400,130, 374,186, 257,219, 213,255, 460,161, 478,36, 544,65, 603,29, 585,115, 526,207, 483,240, 414,252, 560,226, 610,186, 601,264, 22,13, 71,20, 136,39, 184,12, 260,12, 325,27, 511,10, 402,11, 566,11
+	
+	pos_gotas_frame3: .word 110,65, 220,80, 240,100, 62,102, 302,71, 380,77, 156,87, 130,143, 206,155, 295,155, 29,201, 83,197, 147,207, 112,260, 53,270, 180,227, 229,193, 346,126, 423,106, 400,160, 374,216, 257,249, 213,285, 460,191, 478,66, 544,95, 603,59, 585,145, 526,237, 483,270, 414,282, 560,256, 610,216, 601,294, 118,35, 228,50, 248,70, 70,72, 310,41, 388,47, 164,57, 138,113, 214,125, 303,125, 37,171, 91,167, 155,177, 120,230, 61,240, 188,197, 237,163, 354,96, 431,76, 408,130, 382,186, 265,219, 221,255, 468,161, 486,36, 552,65, 611,29, 593,115, 534,207, 491,240, 422,252, 568,226, 618,186, 609,264, 30,13, 79,20, 144,39, 192,12, 268,12, 333,27, 519,10, 410,11, 574,11
+
+	
+	// se agrupan de a dos, el primero para x segundo para y por ej 62, 52 es x=52, y=62 y asi
+	// despues se recorre el array haciendo indice * 8 osea lsl #3
+	
 	.equ SCREEN_WIDTH, 		640
 	.equ SCREEN_HEIGH, 		480
 	.equ BITS_PER_PIXEL,  	32
-
-	.equ GPIO_BASE,      0x3f200000
-	.equ GPIO_GPFSEL0,   0x00
-	.equ GPIO_GPLEV0,    0x34
-
+	.equ CANT_GOTAS_F1,	34
+	.equ CANT_GOTAS_F2,	43
+	.equ CANT_GOTAS_F3,	77
 	.globl main
-
+	
+	.text
 main:
 	// x0 contiene la direccion base del framebuffer
  	mov x20, x0	// Guarda la dirección base del framebuffer en x20
-	//---------------- CODE HERE ------------------------------------
 
-	movz x10, 0xC7, lsl 16
-	movk x10, 0x1585, lsl 00
+loop_frames:
+	///// gotas frame 1/////
 
-	mov x2, SCREEN_HEIGH         // Y Size
-loop1:
-	mov x1, SCREEN_WIDTH         // X Size
-loop0:
-	stur w10,[x0]  // Colorear el pixel N
-	add x0,x0,4	   // Siguiente pixel
-	sub x1,x1,1	   // Decrementar contador X
-	cbnz x1,loop0  // Si no terminó la fila, salto
-	sub x2,x2,1	   // Decrementar contador Y
-	cbnz x2,loop1  // Si no es la última fila, salto
+	bl fondo
 
-	// Ejemplo de uso de gpios
-	mov x9, GPIO_BASE
+	bl dibujar_frisk
 
-	// Atención: se utilizan registros w porque la documentación de broadcom
-	// indica que los registros que estamos leyendo y escribiendo son de 32 bits
-
-	// Setea gpios 0 - 9 como lectura
-	str wzr, [x9, GPIO_GPFSEL0]
-
-	// Lee el estado de los GPIO 0 - 31
-	ldr w10, [x9, GPIO_GPLEV0]
-
-	// And bit a bit mantiene el resultado del bit 2 en w10
-	and w11, w10, 0b10
-
-	// w11 será 1 si había un 1 en la posición 2 de w10, si no será 0
-	// efectivamente, su valor representará si GPIO 2 está activo
-	lsr w11, w11, 1
-
-	//---------------------------------------------------------------
+	ldr x3, =pos_gotas
+	add x21, xzr, xzr
+	add x21, x21, CANT_GOTAS_F1
+	movz x9, 0x96	,lsl 16
+	movk x9, 0x99B5	,lsl 0
+	bl fn_gotas
+	
+	sub sp, sp, #24
+	stur x4, [sp, #0]
+	stur x5, [sp, #8]
+	stur x6, [sp, #16]
+	movz x9, 0x00	,lsl 16
+	movk x9, 0x000E	,lsl 0
+	mov x4, #15				// radio
+	mov x5, #502					// x
+	mov x6, #285					// y
+	bl circulo
+	mov x4, #20				// radio
+	mov x5, #175					// x
+	mov x6, #278					// y
+	bl circulo
+	ldur x6, [sp, #16]
+	ldur x5, [sp, #8]
+	ldur x4, [sp, #0]
+	add sp, sp, #24
+		
+	bl fn_delay
+	
+	///// gotas frame 2 /////
+	
+	bl fondo			// redibuja fondo para borrar las gotas viejas
+	
+	bl dibujar_frisk		// mismo q fondo
+	
+	ldr x3, =pos_gotas_frame2
+	add x21, xzr, xzr
+	add x21, x21, CANT_GOTAS_F2		
+	movz x9, 0x96	,lsl 16
+	movk x9, 0x99B5	,lsl 0
+	bl fn_gotas
+	
+	sub sp, sp, #24
+	stur x4, [sp, #0]
+	stur x5, [sp, #8]
+	stur x6, [sp, #16]
+	movz x9, 0x00	,lsl 16
+	movk x9, 0x000E	,lsl 0
+	mov x4, #20				// radio
+	mov x5, #502					// x
+	mov x6, #285					// y
+	bl circulo
+	mov x4, #20				// radio
+	mov x5, #175					// x
+	mov x6, #278					// y
+	bl circulo
+	ldur x6, [sp, #16]
+	ldur x5, [sp, #8]
+	ldur x4, [sp, #0]
+	add sp, sp, #24
+	
+	bl fn_delay
+	
+	///// gotas frame 3 /////
+	
+	bl fondo
+	
+	bl dibujar_frisk
+	
+	ldr x3, =pos_gotas_frame3
+	add x21, xzr, xzr
+	add x21, x21, CANT_GOTAS_F3
+	movz x9, 0x96	,lsl 16
+	movk x9, 0x99B5	,lsl 0
+	bl fn_gotas
+	
+	sub sp, sp, #24
+	stur x4, [sp, #0]
+	stur x5, [sp, #8]
+	stur x6, [sp, #16]
+	movz x9, 0x00	,lsl 16
+	movk x9, 0x000E	,lsl 0
+	mov x4, #30				// radio
+	mov x5, #502					// x
+	mov x6, #293					// y
+	bl circulo
+	mov x4, #30				// radio
+	mov x5, #175					// x
+	mov x6, #278					// y
+	bl circulo
+	ldur x6, [sp, #16]
+	ldur x5, [sp, #8]
+	ldur x4, [sp, #0]
+	add sp, sp, #24
+	
+	bl fn_delay
+	
+	b loop_frames
+ 	
 	// Infinite Loop
-
-InfLoop:
-	b InfLoop
+infloop:
+	b infloop
---
+++ ejercicio2/funciones.s
@@ -0,0 +1,211 @@
+	.global calcular_dir_pixel
+	.global cuadrado
+	.global circulo
+	.global fn_gotas
+	.global fn_delay
+	
+	.equ SCREEN_WIDTH, 640
+	.equ SCREEN_HEIGHT, 480
+	.equ BITS_PER_PIXEL, 32
+	.equ GOTA_W, 3 			// width 
+	.equ GOTA_H, 19			// height
+	
+	
+	// Dirección = Dirección de inicio + 4 * [x + (y * 640)]
+calcular_dir_pixel:
+	// x5 -> x
+	// x6 -> y
+	// x0 (aca devuelve la pos (x,y))
+
+	mov x0, SCREEN_WIDTH				// ancho de la pantalla
+	mul x0, x0, x6					// y * 640
+	add x0, x5, x0					// x + (y * 640)
+	lsl x0, x0, #2					// 4 * [x + (y * 640)]
+	add x0, x20, x0					// dir de inicio(del framebuffer) + 4 * [x + (y * 640)]
+ret
+
+	//es cuadrado pero serviria para rectangulos tmbn
+cuadrado:
+	// x1 -> ancho del cuadrado	
+	// x2 -> altura del cuadrado
+	// x5 -> x (esquina sup izq)
+	// x6 -> y (esquina sup izq)
+	// x9(color) x10 x11 x14 x15  
+	
+	sub sp, sp, #8					// lugar para LR ya que vamos a hacer otro bl
+	stur x30, [sp, #0]				
+	
+	bl calcular_dir_pixel				// devuelve la dir en x0
+	mov x14, x0					// x14 = dir inicial calculada
+	
+	mov x3, SCREEN_WIDTH				// x3 = 640
+	lsl x3, x3, #2					// 2560 = 640*4, osea una fila entera			
+	
+	mov x11, x2					// x11 = altura
+loop_filas:
+	mov x10, x1					// x10 = ancho
+	mov x15, x14					// guardamos dir inicial fila actual
+	pintar_cuadrado:
+		stur w9, [x14]				// el color que tiene w9 a x14
+		add x14, x14, #4			// siguiente pixel
+		sub x10, x10, #1			// ancho - 1
+		cbnz x10, pintar_cuadrado		// hasta que el ancho sea 0, osea que termino de pintar esa fila
+		mov x14, x15				// devolvemos dir inicial a x14
+		add x14, x14, x3 			// pasamos a la dir inicial de la siguiente fila
+		sub x11, x11, #1			// altura - 1
+		cbnz x11, loop_filas			// hasta que altura sea 0
+		
+	ldur x30, [sp, #0]
+	add sp, sp, #8
+ret 
+
+
+	//hacer un cuadrado que encierre al circulo, osea el ancho del cuadrado es el diametro del circulo (radio*2)
+	// el proceso seria con el centro calcular la posicion con calcular_dir_pixel, despues calcular la distancia
+	// (x5-x10)²+(x6-x11)² <= r² 
+	// x10 = x5-r 
+	// x11 = x6-r 
+	// asi y (x5-r,x6-r) es arriba izq del "cuadrado que encierra al circulo"
+	// y de ahi empieza a hacer los triangulos para comparar su hipotenusa con r² (r² seria la hipotenusa de los triang q entran en el circulo)
+	
+circulo:
+	// x4 -> radio 
+	// x5 -> x
+	// x6 -> y
+	// (x5,x6) = centro del circulo
+	// x10 -> pos actual x
+	// x11 -> pos actual y
+	// x9 color
+	// x12 x13 x14 x15
+	
+	sub sp, sp, #8
+	stur x30, [sp, #0]
+	
+	subs x10, x5, x4				// x10 = x5-r
+	b.lt pos_minima_x				// si x5 < x4, osea x10 < 0 
+	b bien
+	
+	pos_minima_x:					//porq si da negativo entonces se va de la pantalla, plt ponemos la minima pos osea 0
+		sub x10, x10, x10			
+		b bien
+	
+	bien:
+		subs x11, x6, x4			// x11 = x6-r
+		b.lt pos_minima_y
+		b bienbien
+		
+	pos_minima_y:
+		sub x11, x11, x11
+		
+	bienbien:
+		add x21, x4, x4				// r+r = diametro    osea el largo de la fila del cuadrado que contiene al circulo
+		mov x22, x21				// largo de filas restantes
+		mov x26, x22				// para reiniciar x21 
+		mov x23, x10				// x inicial (que se va modificando)
+		mov x24, x11				// y inicial (que se va modificando)
+		
+
+loop_y: 
+	cbz x22, end_circulo				// cuando x22 = 0 entonces no hay mas filas, plt se termino el circulo
+	mov x23, x10					// reset x inicial al inicio de la fila
+	mov x21, x26					// reset largo del diametro
+
+					
+	loop_x:
+		cbz x21, next_fila			// terminamos la fila actual	
+		
+		mul x12, x4, x4				// r*r
+		sub x13, x5, x23			// xcentro - xactual
+		sub x14, x6, x24			// ycentro - yactual
+		mul x13, x13, x13				
+		mul x14, x14, x14
+		add x15, x13, x14			// (x5-x10)²+(x6-x11)²
+
+		cmp x15, x12 				// si (x5-x10)²+(x6-x11)² <= r²
+		b.gt next_pixel				// si el pixel esta afuera del circulo entonces no lo pintamos
+		bl pintar_pixel				// si esta adentro
+
+next_pixel:
+	add x23, x23, #1				// siguiente x osea columna de la fila actual
+	sub x21, x21, #1				// decrementar el contador de columna de la fila actual
+	b loop_x
+	
+next_fila:
+	add x24, x24, #1				// ir a la siguiente fila
+	sub x22, x22, #1				// decrementar en 1 el contador de filas
+	b loop_y
+	
+end_circulo:
+	ldur x30, [sp, #0]
+	add sp, sp, #8
+ret
+
+pintar_pixel:
+	// x5 -> x
+	// x6 -> y
+	// x9 color
+	
+	sub sp, sp, #32
+	stur x30, [sp, #0]
+	stur x5, [sp, #8]
+	stur x6, [sp, #16]
+	
+	mov x5, x23
+	mov x6, x24
+	
+	bl calcular_dir_pixel
+	
+	stur w9, [x0]
+	
+	ldur x6, [sp, #16]
+	ldur x5, [sp, #8]
+	ldur x30, [sp, #0]
+	add sp, sp, #32
+ret
+
+fn_gotas:
+	// basicamente un rectangulo pero con ancho y altura fija
+	// x -> x5
+	// y -> x6
+						// x3 puntero al array pos_gotas
+	add x12, xzr, xzr			// indice i para el loop
+	
+	sub sp, sp, #16
+	stur x30, [sp, #0]
+	stur x3, [sp, #8]
+	
+	
+	loop_gotas:
+		cmp x12, x21
+		b.ge end_loop_gotas
+		
+		lsl x7, x12, #3			// i * 8 = inicio de siguiente elemento (si tomamos como 1 elemento a x e y) y seria con offset #32 
+		add x4, x3, x7			// direccion pos_gotas[i]
+		
+		ldur w5, [x4, #0]		
+		ldur w6, [x4, #4]		// offset 4 porq las direcciones "digamos" 0, 1, 2, 3 (osea 32bits) son para x y despues direccionws 4, 5, 6, 7 empezaria y 
+		
+		mov x1, GOTA_W
+		mov x2, GOTA_H
+		bl cuadrado
+		
+		ldur x3, [sp, #8]
+		
+		add x12, x12, #1
+		b loop_gotas
+	
+	end_loop_gotas:
+		ldur x30, [sp, #0]
+		add sp, sp, #16
+ret
+
+fn_delay:
+	// x8 = duracion delay
+	movz x8, 0x0FFF, lsl 16
+	movk x8, 0xFFFF, lsl 0
+	
+	delay:
+		sub x8, x8, #1
+		cbnz x8, delay
+ret
+
